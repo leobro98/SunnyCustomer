33be64b7244ff34413907b3cf615e40ee0fc6a4b
Leonid Brodski <leob@hot.ee>
2026-08-04 16:44:04 +0200

    UI refactoring

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,88 +2,12 @@
 	<div class="container">
 		<h1>Sunny Customer</h1>
 
-		<div class="form">
-			<table>
-				<tr>
-					<td>
-						<div>
-							<label>First name</label>
-							<input v-model="firstName">
-						</div>
-					</td>
-					<td class="error">
-						{{ errors.firstName }}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div>
-							<label>Last name</label>
-							<input v-model="lastName">
-						</div>
-					</td>
-					<td class="error">
-						{{ errors.lastName }}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div>
-							<label>Birth date</label>
-							<input type="date" v-model="birthDate">
-						</div>
-					</td>
-					<td class="error">
-						{{ errors.birthDate }}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div>
-							<label>Username</label>
-							<input v-model="userName">
-						</div>
-					</td>
-					<td class="error">
-						{{ errors.userName }}
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<div class="password-row">
-							<div v-if="editId === null">
-								<label>Password</label>
-								<input type="password" v-model="password">
-							</div>
-						</div>
-					</td>
-					<td class="error">
-						<div class="password-row">
-							{{ errors.password }}
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<div v-if="formError" class="form-error">
-							{{ formError }}
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<div class="button-row">
-							<button @click="submitForm()">
-								{{ editId === null ? 'Create' : 'Save' }}
-							</button>
-							<button v-if="editId !== null" @click="clearForm()">
-								Cancel
-							</button>
-						</div>
-					</td>
-				</tr>
-			</table>
-		</div>
+		<CustomerForm
+				v-model:customer="formCustomer"
+				:form-error="formError"
+				@submit="submitForm"
+				@cancel="clearForm"
+		/>
 
 		<CustomerTable
 				:customers="customers"
@@ -97,24 +21,17 @@
 import {onMounted, ref, reactive} from 'vue';
 import CustomerTable from './components/CustomerTable.vue';
 import CustomerApi from "./services/CustomerApi.js";
+import CustomerForm from "@/components/CustomerForm.vue";
 
 const customers = ref([]);
-
-const firstName = ref('');
-const lastName = ref('');
-const birthDate = ref('');
-const userName = ref('');
-const password = ref('');
-const editId = ref(null);
-
-const errors = reactive({
+const formCustomer = reactive({
+	id: null,
 	firstName: '',
 	lastName: '',
 	birthDate: '',
 	userName: '',
 	password: '',
 });
-
 const formError = ref('');
 
 async function reloadCustomers() {
@@ -126,53 +43,57 @@ onMounted(async () => {
 });
 
 function clearForm() {
-	firstName.value = '';
-	lastName.value = '';
-	birthDate.value = '';
-	userName.value = '';
-	password.value = '';
+	formCustomer.id = null;
+	formCustomer.firstName = '';
+	formCustomer.lastName = '';
+	formCustomer.birthDate = '';
+	formCustomer.userName = '';
+	formCustomer.password = '';
 
-	editId.value = null;
-
-	clearErrors();
 	formError.value = '';
 }
 
 function createNewCustomerRequest() {
 	return {
-		first_name: firstName.value,
-		last_name: lastName.value,
-		birth_date: birthDate.value,
-		user_name: userName.value,
-		password: password.value
+		first_name: formCustomer.firstName,
+		last_name: formCustomer.lastName,
+		birth_date: formCustomer.birthDate,
+		user_name: formCustomer.userName,
+		password: formCustomer.password
 	};
 }
 
 function createUpdatedCustomerRequest() {
 	return {
-		first_name: firstName.value,
-		last_name: lastName.value,
-		birth_date: birthDate.value,
-		user_name: userName.value
+		first_name: formCustomer.firstName,
+		last_name: formCustomer.lastName,
+		birth_date: formCustomer.birthDate,
+		user_name: formCustomer.userName
 	};
 }
 
-async function submitForm() {
-	clearErrors();
+function editCustomer(customer) {
 	formError.value = '';
 
-	if (!validate()) {
-		return;
-	}
+	formCustomer.id = customer.id;
+	formCustomer.firstName = customer.firstName;
+	formCustomer.lastName = customer.lastName;
+	formCustomer.birthDate = customer.birthDate;
+	formCustomer.userName = customer.userName;
+	formCustomer.password = '';
+}
+
+async function submitForm() {
+	formError.value = '';
 
 	try {
-		if (editId.value === null) {
+		if (formCustomer.id === null) {
 			await CustomerApi.createCustomer(
 					createNewCustomerRequest()
 			);
 		} else {
 			await CustomerApi.updateCustomer(
-					editId.value,
+					formCustomer.id,
 					createUpdatedCustomerRequest()
 			);
 		}
@@ -186,60 +107,19 @@ async function submitForm() {
 	}
 }
 
-function editCustomer(customer) {
-	clearErrors();
-	formError.value = '';
-
-	editId.value = customer.id;
-
-	firstName.value = customer.firstName;
-	lastName.value = customer.lastName;
-	birthDate.value = customer.birthDate;
-	userName.value = customer.userName;
-	password.value = '';
-}
-
 async function deleteCustomer(customer) {
 	if (!confirm(`Delete customer "${customer.firstName} ${customer.lastName}"?`)) {
 		return;
 	}
 
-	await CustomerApi.deleteCustomer(customer.id);
-	await reloadCustomers();
-}
-
-function clearErrors() {
-	errors.firstName = '';
-	errors.lastName = '';
-	errors.birthDate = '';
-	errors.userName = '';
-	errors.password = '';
-}
-
-function validate() {
-	let isValid = true;
-	clearErrors();
-
-	if (!firstName.value.trim()) {
-		errors.firstName = 'First name is required.';
-		isValid = false;
+	try {
+		await CustomerApi.deleteCustomer(customer.id);
+		await reloadCustomers();
+	} catch (error) {
+		formError.value = error instanceof Error
+				? error.message
+				: 'Unknown error.';
 	}
-	if (!lastName.value.trim()) {
-		errors.lastName = 'Last name is required.';
-		isValid = false;
-	}
-	if (!birthDate.value.trim()) {
-		errors.birthDate = 'Birth date is required.';
-	}
-	if (!userName.value.trim()) {
-		errors.userName = 'Username is required.';
-		isValid = false;
-	}
-	if (editId.value === null && !password.value.trim()) {
-		errors.password = 'Password is required.';
-		isValid = false;
-	}
-	return isValid;
 }
 </script>
 
@@ -252,36 +132,9 @@ input {
 	width: 250px;
 }
 
-
-.button-row {
-	display: flex;
-	gap: 10px;
-}
-.button-row > button {
-	width: 80px;
-}
 .container {
 	width: 900px;
 	margin: 40px auto;
 	font-family: Arial, sans-serif;
-}
-.error {
-	width: 220px;
-	padding-left: 10px;
-	color: #d32f2f;
-}
-.form {
-	margin-bottom: 30px;
-}
-.form div {
-	margin-bottom: 5px;
-}
-.form-error {
-	color: #d32f2f;
-	font-weight: bold;
-	margin-bottom: 10px;
-}
-.password-row {
-	min-height: 30px;
 }
 </style>
---
+++ frontend/src/components/CustomerForm.vue
@@ -0,0 +1,190 @@
+<template>
+	<div class="form">
+		<table>
+			<tr>
+				<td>
+					<div>
+						<label>First name</label>
+						<input v-model="customer.firstName"/>
+					</div>
+				</td>
+				<td class="error">
+					{{ errors.firstName }}
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div>
+						<label>Last name</label>
+						<input v-model="customer.lastName">
+					</div>
+				</td>
+				<td class="error">
+					{{ errors.lastName }}
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div>
+						<label>Birth date</label>
+						<input type="date" v-model="customer.birthDate">
+					</div>
+				</td>
+				<td class="error">
+					{{ errors.birthDate }}
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div>
+						<label>Username</label>
+						<input v-model="customer.userName">
+					</div>
+				</td>
+				<td class="error">
+					{{ errors.userName }}
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="password-row">
+						<div v-if="customer.id === null">
+							<label>Password</label>
+							<input type="password" v-model="customer.password">
+						</div>
+					</div>
+				</td>
+				<td class="error">
+					<div class="password-row">
+						{{ errors.password }}
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<div v-if="formError" class="form-error">
+						{{ formError }}
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<div class="button-row">
+						<button @click="handleSubmit()">
+							{{ customer.id === null ? 'Create' : 'Save' }}
+						</button>
+						<button v-if="customer.id !== null" @click="clearForm()">
+							Cancel
+						</button>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const customer = defineModel('customer');
+
+const props = defineProps({
+	formError: String
+});
+
+const errors = reactive({
+	firstName: '',
+	lastName: '',
+	birthDate: '',
+	userName: '',
+	password: '',
+});
+
+const emit = defineEmits([
+	'submit',
+	'cancel',
+]);
+
+function handleSubmit() {
+	if (validate()) {
+		emit('submit');
+	}
+}
+
+function validate() {
+	let isValid = true;
+	clearErrors();
+
+	if (!customer.value.firstName.trim()) {
+		errors.firstName = 'First name is required.';
+		isValid = false;
+	}
+	if (!customer.value.lastName.trim()) {
+		errors.lastName = 'Last name is required.';
+		isValid = false;
+	}
+	if (!customer.value.birthDate.trim()) {
+		errors.birthDate = 'Birth date is required.';
+		isValid = false;
+	}
+	if (!customer.value.userName.trim()) {
+		errors.userName = 'Username is required.';
+		isValid = false;
+	}
+	if (customer.value.id === null && !customer.value.password.trim()) {
+		errors.password = 'Password is required.';
+		isValid = false;
+	}
+	return isValid;
+}
+
+function clearForm() {
+	customer.value.firstName = '';
+	customer.value.lastName = '';
+	customer.value.birthDate = '';
+	customer.value.userName = '';
+	customer.value.password = '';
+
+	clearErrors();
+	emit('cancel');
+}
+
+function clearErrors() {
+	errors.firstName = '';
+	errors.lastName = '';
+	errors.birthDate = '';
+	errors.userName = '';
+	errors.password = '';
+}
+</script>
+
+<style scoped>
+.button-row {
+	display: flex;
+	gap: 10px;
+}
+.button-row > button {
+	width: 80px;
+}
+.error {
+	width: 220px;
+	padding-left: 10px;
+	color: #d32f2f;
+	font-size: 0.9rem;
+}
+.form {
+	margin-bottom: 30px;
+}
+.form div {
+	margin-bottom: 5px;
+}
+.form-error {
+	color: #d32f2f;
+	font-weight: bold;
+	margin-top: 10px;
+	margin-bottom: 10px;
+}
+.password-row {
+	min-height: 30px;
+}
+</style>
